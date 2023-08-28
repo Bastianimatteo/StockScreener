d9d0b9fdc61d5e021ae9e3d3be8bdf5d8dad54a0
Bastianimatteo <110730345+Bastianimatteo@users.noreply.github.com>
2023-08-28 17:13:43 +0200

    Update Screener.js

--- Screener.js
+++ Screener.js
@@ -1,33 +1,23 @@
 array = ["UCG.MI", "ISP.MI", "ENEL.MI", "ENI.MI", "LVMH.MI", "FBK.MI", "STLAM.MI", 
-"AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NIO", "MU", "ASML", "NFLX", "QCOM", "RIVN", "RBLX", "NVDA", "U", "AMD", "META", "INTC", "PARA", "PYPL",
-"PLUG", "NKLA", "BLDP", "BE", "FCEL", "SPWR", "NEL.OL", "RUN", "CSIQ", "ENPH", "DQ", "FSLR", 
+"AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "TM", "RACE", "GM", "NIO", "MU", "ASML", "NFLX", "QCOM", "RIVN", "RBLX", "NVDA", "U", "AMD", "META", "INTC", "PARA", "PYPL",
+"PLUG", "NKLA", "BLDP", "BE", "FCEL", "SPWR", "NEL.OL", "RUN", "CSIQ", "ENPH", "DQ", "FSLR", "SEDG", "JKS", "0916.HK",
 "BYND", "BAC", "KO", "GS", "JPM", "CGC", "TSM", 
-"PFE", "SNY", "NVS", "JNJ", "BNTX", "MRNA", "NTLA", "BIIB", "GSK", "ROG.SW", "PG", 
-"AEM", "PSA", "SBSW", "BTI", "OGI", "VZ", "RYAAY"];
-
-array_crypto = ["BTC-USD", "ETH-USD", "SOL-USD", "CRO-USD", "BNB-USD", "XRP-USD", "ADA-USD"];
+"PFE", "SNY", "NVS", "JNJ", "BNTX", "MRNA", "NTLA", "BIIB", "GSK", "ROG.SW", "CRSP", "LLY", "MRK", "AZN", "ABBV", "PG", 
+"AEM", "PSA", "SBSW", "BTI", "OGI", "TLRY", "CRON", "VZ", "RYAAY", "BA", "AIR.PA"];
 
 var xhttp = [];
-var xhttp_crypto = [];
 
 var response
-var response_crypto = [];
 
 var data = [];
-var dat = {name: "", price: null, market_cap: null, gross_margin: null, profit_margin: null, roe: null, debt_to_equity: null, dividend_yield: null, pe: null, pb: null, current_ratio: null, change: null}
-var data_crypto = [];
+var dat = {name: "", price: null, market_cap: null, gross_margin: null, profit_margin: null, roe: null, debt_to_equity: null, dividend_yield: null, pe: null, pb: null, current_ratio: null, change: null, rev: [], net: [], gross: [], profit: [], debt: [], debtequity: [], roe1: [], roa1: []}
 
 var sorted_data = []
 
 var tb = document.getElementById("tbody");
-var tb_crypto = document.getElementById("tbody_crypto");
 
 var mancanti = array.length;
-var mancanti_crypto = array_crypto.length;
 var full = false;
-
-const d_left = document.getElementById("div_left").style;
-const d_crypto = document.getElementById("div_crypto").style;
 
 const modal_balance = document.getElementById("modal_balance")
 modal_balance.style.visibility = "hidden"
@@ -37,7 +27,6 @@ let retryCount = 0;
 
 var already = []
 
-page_size();
 load()
 
 function load(){
@@ -61,31 +50,14 @@ function load(){
                 {
                     
                     already[i] =  1
-                    dat = {name: "", price: null, market_cap: null, gross_margin: null, profit_margin: null, roe: null, debt_to_equity: null, dividend_yield: null, pe: null, pb: null, current_ratio: null, change: null, rev: [], net: [], gross: [], debt: [], equity: [], invest: []}
+                    dat = {name: "", price: null, market_cap: null, gross_margin: null, profit_margin: null, roe: null, debt_to_equity: null, dividend_yield: null, pe: null, pb: null, current_ratio: null, change: null, rev: [], net: [], gross: [], profit: [], debt: [], debtequity: [], roe1: [], roa1: []}
 
                     mancanti --;
 
                     response = JSON.parse(xhttp[i].response);
                     var result = response.quoteSummary?.result[0]
 
-                    const name = result?.price?.shortName || ""
-                    const price = result?.price?.regularMarketPrice?.raw || 0
-                    const market_cap = result?.price?.marketCap?.raw || 0
-                    const change = result?.price?.regularMarketChangePercent?.raw || 0
-
-                    const gross_margin = result?.financialData?.grossMargins?.raw || 0
-                    const profit_margin = result?.financialData?.profitMargins?.raw || 0
-                    const current_ratio = result?.financialData?.currentRatio?.raw || 0
-                    const roe = result?.financialData?.returnOnEquity?.raw || 0
-                    const debt_to_equity = result?.financialData?.debtToEquity?.raw || 0
-
-                    const pe = result?.summaryDetail?.trailingPE?.raw || 0
-                    const dividend_yield = result?.summaryDetail?.trailingAnnualDividendYield?.raw || 0
-
-                    const pb = result?.defaultKeyStatistics?.priceToBook?.raw || 0
-
                     const income = result?.incomeStatementHistory?.incomeStatementHistory || 0
-
                     const balance = result?.balanceSheetHistory?.balanceSheetStatements || 0
 
                     var tmp_rev = []
@@ -115,24 +87,46 @@ function load(){
                         }
                     }
 
+                    var arr_gross = calc_gross(tmp_gross, tmp_rev)
+                    var arr_profit = calc_profit(tmp_net, tmp_rev)
+                    var arr_debtequity = calc_debtequity(tmp_debt, tmp_equity)
+                    var arr_roe = calc_roe(tmp_net, tmp_equity)
+                    var arr_roa = calc_roa(tmp_net, tmp_invest)
+
+                    const name = result?.price?.shortName || ""
+                    const price = result?.price?.regularMarketPrice?.raw || 0
+                    const market_cap = result?.price?.marketCap?.raw || 0
+                    const change = result?.price?.regularMarketChangePercent?.raw || 0
+                    const current_ratio = result?.financialData?.currentRatio?.raw || 0
+                    const pb = result?.defaultKeyStatistics?.priceToBook?.raw || 0
+                    const dividend_yield = result?.summaryDetail?.trailingAnnualDividendYield?.raw || 0
+                    const gross_margin = result?.financialData?.grossMargins?.raw || 0
+                    const profit_margin = result?.financialData?.profitMargins?.raw || 0
+                    const pe = result?.summaryDetail?.trailingPE?.raw || 0
+                    const roe = result?.financialData?.returnOnEquity?.raw || 0
+                    const debt_to_equity = result?.financialData?.debtToEquity?.raw || 0
+
                     dat.name = name
                     dat.price = price.toFixed(2)
                     dat.market_cap = (market_cap / 1000000000).toFixed(2)
                     dat.change = (change * 100).toFixed(2)
-                    dat.gross_margin = (gross_margin * 100).toFixed(2)
-                    dat.profit_margin = (profit_margin * 100).toFixed(2)
+                    dat.gross_margin = gross_margin != 0 ? (gross_margin * 100).toFixed(2) : arr_gross[0]
+                    dat.profit_margin = profit_margin != 0 ? (profit_margin * 100).toFixed(2) : arr_profit[0]
                     dat.current_ratio = current_ratio.toFixed(2)
-                    dat.roe = (roe * 100).toFixed(2)
-                    dat.debt_to_equity = (debt_to_equity / 100).toFixed(2)
+                    dat.roe = roe != 0 ? (roe * 100).toFixed(2) : arr_roe[0]
+                    dat.debt_to_equity = debt_to_equity != 0 ? (debt_to_equity / 100).toFixed(2) : arr_debtequity[0]
                     dat.pe = (pe).toFixed(2)
                     dat.dividend_yield = (dividend_yield * 100).toFixed(2)
                     dat.pb = (pb).toFixed(2)
+
                     dat.rev = tmp_rev
                     dat.net = tmp_net
-                    dat.gross = tmp_gross
+                    dat.gross = arr_gross
+                    dat.profit = arr_profit
                     dat.debt = tmp_debt
-                    dat.equity = tmp_equity
-                    dat.invest = tmp_invest
+                    dat.debtequity = arr_debtequity
+                    dat.roe1 = arr_roe
+                    dat.roa1 = arr_roa
 
                     data.push(dat)
 
@@ -157,7 +151,7 @@ function load(){
 
 function retryRequest(i) 
 {
-    if (retryCount < 30) 
+    if (retryCount < 20) 
     {
       retryCount++
       setTimeout(() => {
@@ -167,28 +161,6 @@ function retryRequest(i)
     else 
     {
         //alert("Network error. AGGIORNA LA PAGINA");
-    }
-}
-
-for(let i= 0; i<array_crypto.length; i++)
-{
-    xhttp_crypto[i] = new XMLHttpRequest();
-    xhttp_crypto[i].open("GET", "https://query2.finance.yahoo.com/v10/finance/quoteSummary/" + array_crypto[i] + "?modules=price");
-    xhttp_crypto[i].send();
-
-    xhttp_crypto[i].onreadystatechange = function()
-    {
-        if(xhttp_crypto[i].status == 200 && xhttp_crypto[i].readyState ==4)
-        {
-            mancanti_crypto --;
-            response_crypto[i] = JSON.parse(xhttp_crypto[i].response);
-            data_crypto[i] = response_crypto[i].quoteSummary.result[0].price;
-
-            if(mancanti_crypto == 0)
-            {
-                table_crypto();
-            }
-        }
     }
 }
 
@@ -237,14 +209,7 @@ function table()
         }
 
         var cell_roe = row.insertCell();
-        if(sorted_data[i].roe != 0)
-        {
-            cell_roe.innerHTML = sorted_data[i].roe + " %";
-        }
-        else
-        {
-            cell_roe.innerHTML = "-";
-        }
+        cell_roe.innerHTML = sorted_data[i].roe != 0 ? sorted_data[i].roe + " %" : "-"        
 
         var cell_debtequity = row.insertCell();
         if(sorted_data[i].debt_to_equity != 0)
@@ -263,7 +228,7 @@ function table()
         }
         else
         {
-            cell_debtequity.innerHTML = "-";
+            cell_currentratio.innerHTML = "-";
         }
 
 
@@ -312,13 +277,11 @@ function table()
     }
         
     colors();
-
-    page_size();
 }
 
 function showModal(i)
 {
-    console.log(sorted_data[i].invest)
+    console.log(sorted_data[i])
 
     modal_balance_title.innerHTML = "<b>" + sorted_data[i].name + "</b>"
 
@@ -338,7 +301,6 @@ function showModal(i)
     for(let j = 0; j<5; j++)
     {
         var cell = row_revenue.insertCell();
-
         j == 0 ? (cell.innerHTML = "Total Revenue") : (cell.innerHTML = sorted_data[i].rev[j - 1] != 0 ? 
         format(sorted_data[i].rev[j - 1]) : "-")
     }
@@ -347,7 +309,6 @@ function showModal(i)
     for(let j = 0; j<5; j++)
     {
         var cell = row_net.insertCell();
-
         j == 0 ? (cell.innerHTML = "Net Income") : (cell.innerHTML = sorted_data[i].net[j - 1] != 0 ? 
         format(sorted_data[i].net[j - 1]) : "-")   
     }
@@ -356,14 +317,14 @@ function showModal(i)
     for(let j = 0; j<5; j++)
     {
         var cell = row_gross_percent.insertCell();
-        j == 0 ? (cell.innerHTML = "<b>Gross Margin %</b>") : cell.innerHTML = (sorted_data[i].gross[j-1] != 0 && sorted_data[i].rev[j-1] != 0) ? ((sorted_data[i].gross[j-1] * 100) / sorted_data[i].rev[j-1]).toFixed(2) + " %" : "-"
+        j == 0 ? (cell.innerHTML = "<b>Gross Margin %</b>") : cell.innerHTML = (sorted_data[i].gross[j-1] != 0) ? sorted_data[i].gross[j-1] + " %" : "-"
     }
 
     const row_profit_percent = tbody_grossmargin.insertRow()
     for(let j = 0; j<5; j++)
     {
         var cell = row_profit_percent.insertCell();
-        j == 0 ? (cell.innerHTML = "<b>Profit Margin %</b>") : cell.innerHTML = (sorted_data[i].net[j-1] != 0 && sorted_data[i].rev[j-1] != 0) ? ((sorted_data[i].net[j-1] * 100) / sorted_data[i].rev[j-1]).toFixed(2) + " %" : "-"
+        j == 0 ? (cell.innerHTML = "<b>Net Profit Margin %</b>") : cell.innerHTML = (sorted_data[i].profit[j-1] != 0) ? (sorted_data[i].profit[j-1]) + " %" : "-"
     }
 
     const row_debt = tbody_debt.insertRow()
@@ -377,21 +338,21 @@ function showModal(i)
     for(let j = 0; j<5; j++)
     {
         var cell = row_debtequity.insertCell();
-        j == 0 ? (cell.innerHTML = "<b>Debt/Equity</b>") : cell.innerHTML = (sorted_data[i].debt[j-1] != 0 && sorted_data[i].equity[j-1] != 0) ? (sorted_data[i].debt[j-1] / sorted_data[i].equity[j-1]).toFixed(2) : "-"
+        j == 0 ? (cell.innerHTML = "<b>Debt/Equity</b>") : cell.innerHTML = (sorted_data[i].debtequity[j-1] != 0) ? (sorted_data[i].debtequity[j-1] ) : "-"
     }
 
     const row_roe = tbody_returns.insertRow()
     for(let j = 0; j<5; j++)
     {
         var cell = row_roe.insertCell();
-        j == 0 ? (cell.innerHTML = "<b>ROE</b>") : cell.innerHTML = (sorted_data[i].net[j-1] != 0 && sorted_data[i].equity[j-1] != 0) ? ((sorted_data[i].net[j-1] / sorted_data[i].equity[j-1]) * 100).toFixed(2) + " %" : "-"
+        j == 0 ? (cell.innerHTML = "<b>ROE</b>") : cell.innerHTML = (sorted_data[i].roe1[j-1] != 0) ? (sorted_data[i].roe1[j-1]) + " %" : "-"
     }
 
-    const row_roi = tbody_returns.insertRow()
+    const row_roa = tbody_returns.insertRow()
     for(let j = 0; j<5; j++)
     {
-        var cell = row_roi.insertCell();
-        j == 0 ? (cell.innerHTML = "<b>ROA</b>") : cell.innerHTML = (sorted_data[i].net[j-1] != 0 && sorted_data[i].invest[j-1] != 0) ? (sorted_data[i].net[j-1] / sorted_data[i].invest[j-1] * 100).toFixed(2) + " %" : "-"
+        var cell = row_roa.insertCell();
+        j == 0 ? (cell.innerHTML = "<b>ROA</b>") : cell.innerHTML = (sorted_data[i].roa1[j-1] != 0) ? (sorted_data[i].roa1[j-1]) + " %" : "-"
     }
 
     modal_balance.style.visibility = "visible"
@@ -401,34 +362,6 @@ function format(value)
 {
     const new_value = Math.abs(value) > 1000000000 ? (value / 1000000000).toFixed(2) + " B" : Math.abs(value) > 1000000 ? (value / 1000000).toFixed(2) + " M" : (value / 1000).toFixed(2) + " K"
     return new_value
-}
-
-function table_crypto()
-{
-    tb_crypto.innerHTML = "";
-
-    for(let i=0; i<data_crypto.length; i++)
-    {
-        var row_crypto = tb_crypto.insertRow();
-
-        var cell_name_crypto = row_crypto.insertCell();
-        cell_name_crypto.innerHTML = data_crypto[i].shortName;
-
-        var cell_price_crypto = row_crypto.insertCell();
-        data_crypto[i].regularMarketPrice.raw > 1 ? (cell_price_crypto.innerHTML = (data_crypto[i].regularMarketPrice.raw).toFixed(2)) : (cell_price_crypto.innerHTML = (data_crypto[i].regularMarketPrice.raw).toFixed(3));
-
-        var cell_change_crypto = row_crypto.insertCell();
-        cell_change_crypto.innerHTML = (data_crypto[i].regularMarketChangePercent.raw *100).toFixed(2);
-    }
-
-    colors_crypto();
-
-    var tr = tb_crypto.getElementsByTagName("tr");
-    for(let i=0; i<tr.length; i++)
-    {
-        var td = tr[i].getElementsByTagName("td");
-        td[2].innerHTML = td[2].textContent + " %";
-    }
 }
 
 function colors()
@@ -461,13 +394,17 @@ function colors()
         {
             td[4].style.color = "#e60000"
         }
+        if(sorted_data[i].roe > 30)
+        {
+            td[4].style.color = "green"
+        }
 
         if(sorted_data[i].debt_to_equity > 1)
         {
             td[5].style.color = "#e60000"
         }
 
-        if(sorted_data[i].current_ratio < 1)
+        if(sorted_data[i].current_ratio != 0 && sorted_data[i].current_ratio < 1)
         {
             td[6].style.color = "#e60000"
         }
@@ -481,25 +418,6 @@ function colors()
             td[11].style.color ="#e60000";
         }
     }
-}
-
-function colors_crypto()
-{
-    var tr = tb_crypto.getElementsByTagName("tr");
-
-    for(let i=0; i<tr.length; i++)
-    {
-        var td = tr[i].getElementsByTagName("td");
-        if(td[2].innerHTML > 0)
-        {
-            td[2].style.color = "green"
-        }
-        else
-        {
-            td[2].style.color ="#e60000";
-        }
-    }
-
 }
 
 function sort(arr)
@@ -570,57 +488,59 @@ function sort(arr)
     }
 }
 
-function page_size()
-{
-    let pageWidth = window.innerWidth;
-    let screenWidth = screen.width;
-
-    let tableHeight = document.getElementById("table").offsetHeight;
-    let table_cryptoHeight = document.getElementById("table_crypto").offsetHeight;
-    let max = Math.max(tableHeight, 700);
-
-    if(screenWidth == pageWidth)  // fullscreen
-    {
-        d_left.width = "75%";
-        d_crypto.width = "17%";
-
-        d_left.marginLeft = "2%";
-        d_crypto.marginLeft = "3%";
-
-        d_left.marginTop = "-1";
-        d_crypto.marginTop = "-1";
-
-        d_left.float = "left";
-        d_crypto.float = "left";
-
-        d_crypto.height = max + "px"
-    }
-    else // half screen
-    {
-        d_left.width = "80%";
-        d_crypto.width = "80%"
-
-        d_left.margin = "auto";
-        d_left.marginTop = "-1";
-
-        d_crypto.margin = "auto";
-        d_crypto.marginTop = "3%";
-
-        d_crypto.height = table_cryptoHeight + "px";
-
-        max = 700;
-    }
-
-    d_left.height = tableHeight + "px";
-}
-
-window.onresize = function() {
-    page_size();
-}
-
 window.onclick = function(event) {
     if(event.target == modal_balance)
     {
         modal_balance.style.visibility = "hidden"
     }
   }
+
+function calc_gross(gross, rev)
+{
+    var a = []
+    for(var i = 0; i<4; i++)
+    {
+        a[i] = (gross[i] != 0 && rev[i] != 0) ? gross[i] != rev[i] ? ((gross[i] * 100) / rev[i]).toFixed(2) : 0 : 0
+    }
+    return a
+}
+
+function calc_profit(net, rev)
+{
+    var a = []
+    for(var i = 0; i<4; i++)
+    {
+        a[i] = (net[i] != 0 && rev[i] != 0) ? ((net[i] * 100) / rev[i]).toFixed(2) : 0
+    }
+    return a
+}
+
+function calc_debtequity(debt, equity)
+{
+    var a = []
+    for(var i = 0; i<4; i++)
+    {
+        a[i] = (debt[i] != 0 && equity[i] != 0) ? (debt[i] / equity[i]).toFixed(2) : 0
+    }
+    return a
+}
+
+function calc_roe(net, equity)
+{
+    var a = []
+    for(var i = 0; i<4; i++)
+    {
+        a[i] = (net[i] != 0 && equity[i] != 0) ? (net[i] / equity[i] * 100).toFixed(2) : 0
+    }
+    return a
+}
+
+function calc_roa(net, invest)
+{
+    var a = []
+    for(var i = 0; i<4; i++)
+    {
+        a[i] = (net[i] != 0 && invest[i] != 0) ? (net[i] / invest[i] * 100).toFixed(2) : 0
+    }
+    return a
+}
